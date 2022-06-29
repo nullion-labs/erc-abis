dc675f0e5ce5f9c479ddc9e4efc65aab92029858
vujable <vukasin@nullion.com>
2022-06-29 13:57:59 -0700

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# ERC ABIS
+
+## Available
+
+- ERC20
+- ERC721
+- ERC1155
---
+++ index.js
@@ -0,0 +1,9 @@
+const ERC20 = require('./abis/ERC20.json');
+const ERC721 = require('./abis/ERC721.json');
+const ERC1155 = require('./abis/ERC1155.json');
+
+module.exports = {
+    ERC20,
+    ERC721,
+    ERC1155
+};
